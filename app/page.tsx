1bdcf304275ec1adddf77f078e400f0bab36ec09
Ayush Thakur <ayshthkr@gmail.com>
2025-06-02 11:44:06 +0530

    Fixed responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -16,27 +16,27 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-2 items-center min-h-screen w-full font-sfpro mt-10 selection:bg-red-500 selection:text-white ">
       {/* <DrawerComponent /> */}
-      <div className="block max-w-[40vw]">
-        <p className="text-4xl font-black animate-in slide-in-from-bottom-2 fade-in delay-75 duration-200 ">AYUSH THAKUR</p>
-        <p className="text-2xl font-medium mt-3">
+      <div className="block md:max-w-[40vw] p-8">
+        <p className="text-2xl md:text-4xl font-black animate-in slide-in-from-bottom-2 fade-in delay-75 duration-200 ">AYUSH THAKUR</p>
+        <p className="text-lg md:text-2xl font-medium mt-3">
           3rd Year B.Tech (CSE) student at Netaji Subhas University of
           Technology
         </p>
-        <p className="text-lg font-normal mt-3">
+        <p className="text-base md:text-lg font-normal mt-3">
           Full-stack developer and AI/ML enthusiast, passionate about building
           performant, user-focused web apps. Exploring the edge of GenAI, dev
           tooling, and visual computing.
         </p>
-        <p className="text-lg font-normal, mt-2">
+        <p className="text-base md:text-lg font-normal, mt-2">
           Want to get in touch, react out via the links below!
         </p>
 
-        <div className="flex gap-4 mt-4">
+        <div className="flex flex-wrap mt-4">
           {Object.entries(links).map(([name, url]) => (
             <Link
               key={name}
               href={url}
-              className="underline hover:decoration-2 hover:text-red-500 mt-2 block transition-all"
+              className="underline mx-2 hover:decoration-2 hover:text-red-500 mt-2 block transition-all"
               target={name != "Designs" ? "_blank" : undefined}
               rel="noopener noreferrer"
             >
@@ -44,7 +44,7 @@ export default function Home() {
             </Link>
           ))}
         </div>
-        <div className="mt-2">
+        <div className="mt-4">
           Email:{" "}
           <Link
             href="mailto:ayshthkr@gmail.com"
@@ -61,13 +61,13 @@ export default function Home() {
               window.open("https://github.com/ayshthkr/ecomitra", "_blank")
             }
           >
-            <p className="text-xl p-4 flex justify-between">
+            <p className="text-xl p-4 flex flex-col md:flex-row md:justify-between">
               <span className="font-bold">EcoMitra</span>{" "}
               <span className="font-mono text-base">
                 NextJS, PostgreSQL, Prisma
               </span>
             </p>
-            <div className="flex p-1">
+            <div className="md:flex p-1">
               <p className="text-base px-4 flex gap-2 flex-1">
                 Built a dashboard that summarized banking statements and
                 improved insight clarity, used by 50+ users. <br />
@@ -79,7 +79,7 @@ export default function Home() {
                 src={"/ecomitra-screenshot.png"}
                 width={1771}
                 height={1068}
-                className="w-[40%] object-contain"
+                className="w-full p-4 md:p-0 md:w-[40%] object-contain"
                 alt="Ecomitra screengrab"
               />
             </div>
@@ -93,7 +93,7 @@ export default function Home() {
               )
             }
           >
-            <p className="text-xl p-4 flex justify-between">
+            <p className="text-xl p-4 flex flex-col md:flex-row md:justify-between">
               <span className="font-bold">
                 Skin Cancer Detection Using EfficientNet
               </span>{" "}
@@ -101,7 +101,7 @@ export default function Home() {
                 PyTorch, EfficientNet, OpenCV
               </span>
             </p>
-            <div className="flex p-1">
+            <div className="md:flex p-1">
               <p className="text-base px-4 flex gap-2 flex-1">
                 Developed and fine-tuned a deep learning model (EfficientNet-B4)
                 for highly accurate skin cancer (melanoma) detection, achieving
@@ -115,7 +115,7 @@ export default function Home() {
                 src={"/skin-cancer-training.png"}
                 width={1771}
                 height={1068}
-                className="w-[40%] object-contain"
+                className="w-full p-4 md:p-0 md:w-[40%] object-contain"
                 alt="Skin Cancer Detection Training"
               />
             </div>
@@ -126,13 +126,13 @@ export default function Home() {
               window.open("https://github.com/ayshthkr/email-client", "_blank")
             }
           >
-            <p className="text-xl p-4 flex justify-between">
+            <p className="text-xl p-4 flex flex-col md:flex-row md:justify-between">
               <span className="font-bold">Email Client</span>{" "}
               <span className="font-mono text-base">
                 NextJS, React, PostgreSQL
               </span>
             </p>
-            <div className="flex p-1">
+            <div className="md:flex p-1">
               <p className="text-base px-4 flex gap-2 flex-1">
                 Developed a robust and responsive frontend interface for a
                 PostgreSQL-based email client, focusing on intuitive user
@@ -146,7 +146,7 @@ export default function Home() {
                 src={"/email-client.png"}
                 width={1771}
                 height={1068}
-                className="w-[40%] object-contain"
+                className="w-full p-4 md:p-0 md:w-[40%] object-contain"
                 alt="Email Client"
               />
             </div>
@@ -154,7 +154,7 @@ export default function Home() {
         </div>
         <div className="mt-6 py-6 animate-in slide-in-from-bottom-3 fade-in delay-150 duration-300">
           <p className="text-2xl font-extrabold">Achievements:</p>
-          <div className="p-6">
+          <div className="p-6 space-y-4">
             <p>
               <span className="font-bold">
                 Innohacks&apos;24 Winner (March 2024)
@@ -172,7 +172,7 @@ export default function Home() {
         </div>
         <div className="mt-6 pb-6 animate-in slide-in-from-bottom-3 fade-in delay-150 duration-300">
           <p className="text-2xl font-extrabold">Co-Curricular Activities:</p>
-          <div className="p-6">
+          <div className="p-6 space-y-4">
             <p>
               <span className="font-bold">
                 Web Dev Mentor (Google Developer Student Clubs, NSUT)
