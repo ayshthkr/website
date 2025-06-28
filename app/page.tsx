897babfd1f4ec298afc1eed056a9bd7c7104bf86
Ayush Thakur <ayshthkr@gmail.com>
2025-06-28 17:13:31 +0530

    Added resume and fixed some UI

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
           >
             <p className="text-sm p-4 flex flex-col md:flex-row md:justify-between">
               <span className="font-bold">EcoMitra</span>{" "}
-              <span className="font-mono text-sm">
+              <span className="font-mono text-[12px]">
                 NextJS, PostgreSQL, Prisma
               </span>
             </p>
@@ -84,6 +84,39 @@ export default function Home() {
               />
             </div>
           </div>
+
+           <div
+            className="bg-gray-100 p-2 m-4 rounded-lg hover:scale-[1.02] hoverr:bg-gray-200 transition-all cursor-pointer"
+            onClick={() =>
+              window.open("https://github.com/ayshthkr/insights", "_blank")
+            }
+          >
+            <p className="text-sm p-4 flex flex-col md:flex-row md:justify-between">
+              <span className="font-bold">Insights</span>{" "}
+              <span className="font-mono text-[12px]">
+                NextJS, PostgreSQL, Gemini/Exa
+              </span>
+            </p>
+            <div className="p-1">
+              <p className="text-sm px-4 flex gap-2 flex-1">
+                Developed an AI-powered full-stack search engine with
+                dynamic query classification (Gemini 2.5 + Exa for web
+                search) and intelligent context refinement.
+                <br />
+                Improved the user experience with source citations and
+                multi-format exports (PDF, Markdown, Word, Text) to
+                support smooth, interactive searching.
+              </p>
+              <Image
+                src={"/insights-ui.png"}
+                width={1771}
+                height={1068}
+                className="w-full p-4 object-contain"
+                alt="Email Client"
+              />
+            </div>
+          </div>
+
           <div
             className="bg-gray-100 p-2 m-4 rounded-lg hover:scale-[1.02] hoverr:bg-gray-200 transition-all cursor-pointer"
             onClick={() =>
@@ -97,7 +130,7 @@ export default function Home() {
               <span className="font-bold">
                 Skin Cancer Detection Using EfficientNet
               </span>{" "}
-              <span className="font-mono text-sm">
+              <span className="font-mono text-[12px]">
                 PyTorch, EfficientNet, OpenCV
               </span>
             </p>
@@ -120,42 +153,12 @@ export default function Home() {
               />
             </div>
           </div>
-          <div
-            className="bg-gray-100 p-2 m-4 rounded-lg hover:scale-[1.02] hoverr:bg-gray-200 transition-all cursor-pointer"
-            onClick={() =>
-              window.open("https://github.com/ayshthkr/email-client", "_blank")
-            }
-          >
-            <p className="text-sm p-4 flex flex-col md:flex-row md:justify-between">
-              <span className="font-bold">Email Client</span>{" "}
-              <span className="font-mono text-sm">
-                NextJS, React, PostgreSQL
-              </span>
-            </p>
-            <div className="p-1">
-              <p className="text-sm px-4 flex gap-2 flex-1">
-                Developed a robust and responsive frontend interface for a
-                PostgreSQL-based email client, focusing on intuitive user
-                experience and efficient handling of email functionalities.
-                <br />
-                Architected the backend utilizing a PostgreSQL database hosted
-                on Supabase, integrating a secure authentication system to
-                manage user access and data with enhanced scalability.
-              </p>
-              <Image
-                src={"/email-client.png"}
-                width={1771}
-                height={1068}
-                className="w-full p-4 object-contain"
-                alt="Email Client"
-              />
-            </div>
-          </div>
+
         </div>
-        <div className="mt-6 py-6 animate-in slide-in-from-bottom-3 fade-in delay-150 duration-300">
-          <p className="text-2xl font-extrabold">Achievements:</p>
+        <div className="mt-8 py-4 animate-in slide-in-from-bottom-3 fade-in delay-150 duration-300">
+          <p className="text-xl font-extrabold">Achievements:</p>
           <div className="p-6 space-y-4">
-            <p>
+            <p className="text-sm">
               <span className="font-bold">
                 Innohacks&apos;24 Winner (March 2024)
               </span>{" "}
@@ -170,10 +173,10 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div className="mt-6 pb-6 animate-in slide-in-from-bottom-3 fade-in delay-150 duration-300">
-          <p className="text-2xl font-extrabold">Co-Curricular Activities:</p>
+        <div className="mt-2 pb-6 animate-in slide-in-from-bottom-3 fade-in delay-150 duration-300">
+          <p className="text-xl font-extrabold">Co-Curricular Activities:</p>
           <div className="p-6 space-y-4">
-            <p>
+            <p className="text-sm">
               <span className="font-bold">
                 Web Dev Mentor (Google Developer Student Clubs, NSUT)
               </span>{" "}
@@ -181,7 +184,7 @@ export default function Home() {
               sessions, and co-built a Next.js + PostgreSQL CMS that streamlined
               event management and cut manual work by ~50%.
             </p>
-            <p>
+            <p className="text-sm">
               <span className="font-bold">Junior Council (Devcomm, NSUT)</span>{" "}
               - Co-developed a gamified DSA platform to boost coding engagement
               and helped build Avinya&apos;25&apos;s animated, responsive website using
